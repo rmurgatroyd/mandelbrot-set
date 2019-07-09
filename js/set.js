ed041baa8cf5b6a9b01dcaa2715c6675b2e276c2
rmurgatroyd <48757964+rmurgatroyd@users.noreply.github.com>
2019-07-09 16:31:55 +0100

    Add files via upload

--- js/set.js
+++ js/set.js
@@ -1,6 +1,51 @@
+var slider = document.querySelectorAll(".slider")
+
+var magnifySlider = document.querySelector("#magnifyRange");
+var magnifyOutput = document.querySelector("#magnify");
+magnifyOutput.innerHTML = magnifySlider.value;
+
+var xSlider = document.querySelector("#xRange");
+var xoutput = document.querySelector("#xCoord");
+xoutput.innerHTML = xSlider.value;
+
+var ySlider = document.querySelector("#yRange");
+var youtput = document.querySelector("#yCoord");
+youtput.innerHTML = ySlider.value;
+
+// var magnify = slider.value;
+var magnifySlide = document.querySelector(".magnifySlide")
+var magnify = magnifySlide.value;
+
+var xCoord = document.querySelector(".xSlide");
+var yCoord = document.querySelector(".ySlide");
+
+// slider.oninput=function(){
+//   output.innerHTML = slider.value;
+//   magnify = slider.value;
+//   console.log(magnify);
+//   return magnify;
+// }
+
+magnifySlider.oninput=function(){
+magnifyOutput.innerHTML = magnifySlider.value;
+}
+
+xSlider.oninput = function(){
+  xoutput.innerHTML = xSlider.value;
+}
+
+ySlider.oninput = function(){
+youtput.innerHTML = ySlider.value;
+
+ }
+
+
+
+
+
 var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
-canvas.height = 1200;
+canvas.height = 800;
 
 var ctx = canvas.getContext("2d");
 
@@ -30,10 +75,19 @@ function belongToSetCheck(x,y){
 return 0;
 }
 
+// var magnify = slider.value;
 
-var magnify = 600;
-var panX = 1.5;
-var panY = 1;
+// slider.onchange = slideChange();
+//
+// slideChange(){
+//   magnify = slider.value
+// }
+
+
+var panX = xCoord.value;
+var panY = yCoord.value;
+
+
 for (var x=0; x < canvas.width; x++){
   for (var y = 0; y < canvas.height; y++){
     var belongToSet = belongToSetCheck(x/magnify - panX,y/magnify - panY);
@@ -55,4 +109,96 @@ for (var x=0; x < canvas.width; x++){
     }
   }
 
+}
+
+
+
+
+magnifySlider.onchange=function(){
+
+  magnify = magnifySlide.value;
+  console.log(magnify);
+
+
+for (var x=0; x < canvas.width; x++){
+  for (var y = 0; y < canvas.height; y++){
+    var belongToSet = belongToSetCheck(x/magnify - panX,y/magnify - panY);
+    // if(belongToSet){
+    //   ctx.fillRect(x,y,1,1);
+    // }
+    // console.log(belongToSet);
+    if(belongToSet===0){
+      ctx.fillStyle="#000000";
+      ctx.fillRect(x,y, 1,1)
+    }
+    else{
+      // console.log(belongToSet);
+      // ctx.fillStyle="hsl(150,90%,"+ belongToSet+10+"%)";
+      ctx.fillStyle="hsl("+belongToSet*2.55+",100%, 60%)";
+      // ctx.fillStyle="hsl("+belongToSet*3.6+",90%, 100%)";
+      // ctx.fillStyle="#ffffff";
+      ctx.fillRect(x,y, 1,1);
+    }
+  }
+
+}
+}
+
+
+xSlider.onchange=function(){
+
+  panX = xCoord.value;
+
+
+for (var x=0; x < canvas.width; x++){
+  for (var y = 0; y < canvas.height; y++){
+    var belongToSet = belongToSetCheck(x/magnify - panX,y/magnify - panY);
+    // if(belongToSet){
+    //   ctx.fillRect(x,y,1,1);
+    // }
+    // console.log(belongToSet);
+    if(belongToSet===0){
+      ctx.fillStyle="#000000";
+      ctx.fillRect(x,y, 1,1)
+    }
+    else{
+      // console.log(belongToSet);
+      // ctx.fillStyle="hsl(150,90%,"+ belongToSet+10+"%)";
+      ctx.fillStyle="hsl("+belongToSet*2.55+",100%, 60%)";
+      // ctx.fillStyle="hsl("+belongToSet*3.6+",90%, 100%)";
+      // ctx.fillStyle="#ffffff";
+      ctx.fillRect(x,y, 1,1);
+    }
+  }
+
+}
+}
+
+ySlider.onchange=function(){
+
+  panY = yCoord.value;
+
+
+for (var x=0; x < canvas.width; x++){
+  for (var y = 0; y < canvas.height; y++){
+    var belongToSet = belongToSetCheck(x/magnify - panX,y/magnify - panY);
+    // if(belongToSet){
+    //   ctx.fillRect(x,y,1,1);
+    // }
+    // console.log(belongToSet);
+    if(belongToSet===0){
+      ctx.fillStyle="#000000";
+      ctx.fillRect(x,y, 1,1)
+    }
+    else{
+      // console.log(belongToSet);
+      // ctx.fillStyle="hsl(150,90%,"+ belongToSet+10+"%)";
+      ctx.fillStyle="hsl("+belongToSet*2.55+",100%, 60%)";
+      // ctx.fillStyle="hsl("+belongToSet*3.6+",90%, 100%)";
+      // ctx.fillStyle="#ffffff";
+      ctx.fillRect(x,y, 1,1);
+    }
+  }
+
+}
 }
